dd37298290ae00adc67ca42348bb7f9c023dd736
Steven Suarez <suarezsteven30@gmail.com>
2023-08-06 15:38:39 -0400

    Slight cleanup and code tidying

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -14,12 +17,14 @@ void error_callback(int error, const char* description)
 
 int main() 
 {
+    // glfw: Initialize and configure, add error cb function
     glfwSetErrorCallback(error_callback);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // glfw: Window creation
     GLFWwindow* window = glfwCreateWindow(800, 600, "MyFirstWindow", NULL, NULL);
     if (window == NULL)
     {
@@ -29,21 +34,25 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // glad: Load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    // Render loop
     while (!glfwWindowShouldClose(window))
     {
-        glfwSwapBuffers(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
         glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
